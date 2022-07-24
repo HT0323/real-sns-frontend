1a48cc01605195e78778d995ce184175508f9caf
HaraT0323 <alra9si916323@gmail.com>
2022-07-25 01:37:20 +0900

    Topbarコンポーネントの雛形作成

---
+++ src/components/topbar/Topbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Real SNS</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="探し物は何ですか?"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIconItem">1</div>
+        <div className="topbarIconItem">2</div>
+      </div>
+      <img src="/assets/person/1.jpg" alt="" className="topbarImg" />
+    </div>
+  );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Topbar from "../../components/topbar/Topbar";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Topbar />
+    </>
+  );
 }
