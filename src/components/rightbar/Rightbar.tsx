3850d2e8eca32d8b4058d7fc6f4c11758cc36e32
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 00:43:31 +0900

    Rightbarコンポーネントのスタイル適用

--- src/components/rightbar/Rightbar.tsx
+++ src/components/rightbar/Rightbar.tsx
@@ -11,15 +11,30 @@ export default function Rightbar() {
             <b>フォロワー限定</b>イベント開催中
           </span>
         </div>
-        <img src="assets/ad.jpeg" alt="" />
+        <img src="assets/ad.jpeg" alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="assets/person/1.jpeg" alt="" />
+              <img
+                src="assets/person/1.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUsername">ddddd</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/2.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">12345</span>
           </li>
         </ul>
       </div>
