8fe212857980ec6e2f1eb3b382ac05434cd4159c
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 00:23:49 +0900

    Postコンポーネントのスタイルを適用

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -7,22 +7,25 @@ export default function Post() {
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
-          <img
-            src="./assets/person/人物アイコン.jpeg"
-            alt=""
-            className="postProfileImg"
-          />
-          <span className="postUserName">test123</span>
-          <span className="postDate">5分前</span>
+          <div className="postTopLeft">
+            <img
+              src="./assets/person/人物アイコン.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUserName">test123</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-        <div className="postRight">
-          <MoreVert />
-        </div>
+
         <div className="postCenter">
           <span className="postText">SNSを作成中</span>
           <img src="./assets/post/1.jpeg" alt="" className="postImg" />
         </div>
-        <div className="PostBottom">
+        <div className="postBottom">
           <div className="postBottomLeft">
             <img src="./assets/heart.png" alt="" className="likeIcon" />
             <span className="postLikeCounter">5人がいいねを押しました</span>
