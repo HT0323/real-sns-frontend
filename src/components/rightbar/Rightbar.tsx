44cc3962ef7b53781a039a9898717a2ffeb1acac
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 00:32:36 +0900

    Rightbarコンポーネントの雛形作成

--- src/components/rightbar/Rightbar.tsx
+++ src/components/rightbar/Rightbar.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import "./Rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar"> Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="./assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中
+          </span>
+        </div>
+        <img src="assets/ad.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/1.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">ddddd</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
