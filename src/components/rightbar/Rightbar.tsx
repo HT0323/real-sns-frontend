f48fd1134bd5eee7accfc0e6916640b0019a932d
HaraT0323 <alra9si916323@gmail.com>
2022-07-25 23:18:19 +0900

    Timelineコンポーネントの雛形作成

--- src/components/rightbar/Rightbar.tsx
+++ src/components/rightbar/Rightbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Rightbar.css";
 
 export default function Rightbar() {
-  return <div>Rightbar</div>;
+  return <div className="rightbar"> Rightbar</div>;
 }
--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./Timeline.css";
 
 export default function Timeline() {
-  return <div>Timeline</div>;
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        <Share />
+        {/* <Post /> */}
+      </div>
+    </div>
+  );
 }
