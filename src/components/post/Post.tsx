3cacfb9082720c0d88666be7db4190bca8fc855c
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 02:11:20 +0900

    投稿データをダミーに差し替え

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,20 +1,37 @@
 import { MoreVert } from "@mui/icons-material";
-import React from "react";
+import React, { FC } from "react";
 import "./Post.css";
+import { Users } from "../../dummyData";
+import userEvent from "@testing-library/user-event";
 
-export default function Post() {
+type PostProps = {
+  id: number;
+  desc: string;
+  photo: string;
+  date: string;
+  userId: number;
+  like: number;
+  comment: number;
+};
+
+type userProps = {
+  id: number;
+  profilePicture: string;
+  username: string;
+};
+
+export const Post: FC<{ post: PostProps }> = ({ post }) => {
+  const user: userProps = Users.filter(
+    (user: userProps) => user.id === post.userId
+  )[0];
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src="./assets/person/人物アイコン.jpeg"
-              alt=""
-              className="postProfileImg"
-            />
-            <span className="postUserName">test123</span>
-            <span className="postDate">5分前</span>
+            <img src={user.profilePicture} alt="" className="postProfileImg" />
+            <span className="postUserName">{user.username}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -22,19 +39,21 @@ export default function Post() {
         </div>
 
         <div className="postCenter">
-          <span className="postText">SNSを作成中</span>
-          <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="./assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">5人がいいねを押しました</span>
+            <span className="postLikeCounter">
+              {post.like}人がいいねを押しました
+            </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">4:コメント</span>
+            <span className="postCommentText">{post.comment}:コメント</span>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Post from "../post/Post";
+import { Post } from "../post/Post";
 import Share from "../share/Share";
 import "./Timeline.css";
+import { Posts } from "../../dummyData";
 
 export default function Timeline() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {Posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   );
