3186e5ece895a350183620067bce2f0ca97acc49
HaraT0323 <alra9si916323@gmail.com>
2022-07-25 22:57:36 +0900

    Sidebarコンポーネントの雛形作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,60 @@
 import React from "react";
+import {
+  Bookmark,
+  Home,
+  MessageRounded,
+  Notifications,
+  Person,
+  Search,
+  Settings,
+} from "@mui/icons-material";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home className="sidebarIcon" />
+            <span className="sidebarListItemText">ホーム</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search className="sidebarIcon" />
+            <span className="sidebarListItemText">検索</span>
+          </li>
+          <li className="sidebarListItem">
+            <Notifications className="sidebarIcon" />
+            <span className="sidebarListItemText">通知</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageRounded className="sidebarIcon" />
+            <span className="sidebarListItemText">メッセージ</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">ブックマーク</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person className="sidebarIcon" />
+            <span className="sidebarListItemText">プロフィール</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings className="sidebarIcon" />
+            <span className="sidebarListItemText">設定</span>
+          </li>
+        </ul>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">test</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
