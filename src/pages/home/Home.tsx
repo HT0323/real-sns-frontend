f12c522d37f33ea7c030a1f26d09f6613cd06f12
HaraT0323 <alra9si916323@gmail.com>
2022-07-25 22:46:46 +0900

    Sidebar, Timeline, Rightbarコンポーネント作成

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Timeline from "../../components/timeline/Timeline";
 import Topbar from "../../components/topbar/Topbar";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
       <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Timeline />
+        <Rightbar />
+      </div>
     </>
   );
 }
