946c14e4d4aa4149cbc067c5683f5259cf9b36c5
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 22:37:10 +0900

    Profileコンポーネントの作成

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Timeline from "../../components/timeline/Timeline";
+import Topbar from "../../components/topbar/Topbar";
+import "./Profile.css";
+
+export const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                src="assets/post/3.jpeg"
+                alt=""
+                className="profileCoverImg"
+              />
+              <img
+                src="assets/person/7.jpeg"
+                alt=""
+                className="profileUserImg"
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">test</h4>
+              <span className="profileInfoDesc">test1234</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Timeline />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
