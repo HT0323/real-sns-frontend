6cb0c63a7cc0fa136278f5967942a24723cfc96f
HaraT0323 <alra9si916323@gmail.com>
2022-07-26 22:43:52 +0900

    Profileコンポーネントにスタイルを適用

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -20,7 +20,7 @@ export const Profile = () => {
                 className="profileCoverImg"
               />
               <img
-                src="assets/person/7.jpeg"
+                src="assets/person/1.jpeg"
                 alt=""
                 className="profileUserImg"
               />
